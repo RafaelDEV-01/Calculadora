3f242b850de3a6399abc920940b4c6b9540f247a
José Rafael <166348491+RafaelDEV-01@users.noreply.github.com>
2024-07-23 21:44:01 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -11,9 +11,7 @@ public class Calculadora {
 		double resposta2 = calc.mult(9.5, 18);
 		double resposta3 = calc.div(2223, 13);
 		double resposta4 = calc.sub(432,261);
-		System.out.printf("Soma:  %.2f \nSoma2: %.2f \nMult:  %.2f \nDiv:   %.2f \nSub:   %.2f",resposta,resposta1,resposta2,resposta3,resposta4);
-		
-		
+		System.out.printf("Soma:  %.2f \nSoma2: %.2f \nMult:  %.2f \nDiv:   %.2f \nSub:   %.2f",resposta,resposta1,resposta2,resposta3,resposta4);			
 	}
 
 }
